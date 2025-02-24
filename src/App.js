2a785a875b9a069518df35a576b1db64ac406a27
ananysagar <sagaranany577@gmail.com>
2025-02-24 19:22:08 +0530

    making form UI ready

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello
+      <div className="form">
+        <div className="login-form">
+          <h2>Login Form</h2>
+          <label>UserName</label>
+          <input type='text' placeholder='User Name'/>
+          <label>Email</label>
+          <input type='email' placeholder='Email'/>
+          <label>Password</label>
+          <input type="password" placeholder='Password'/>
+          <button type='submit'>Submit</button>
+        </div>
+      </div>
     </div>
   );
 }
